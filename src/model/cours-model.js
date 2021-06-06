dbed7967269f39c9c0a34622d3f1a13571d85ef7
reemalqurm <reem.alqurm@gmail.com>
2021-06-06 13:30:26 +0300

    course schema is done

--- src/model/cours-model.js
+++ src/model/cours-model.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose');
 const course = new mongoose.Schema({
   name: { type: String, required: true, unique: true },
   assignment: { type: Array, required: true },
+  quiz: { type: Array, required: true },
   member: { type: Array, required: true },
   owner: { type: String, required: true },
   description: { type: String, required: true }
