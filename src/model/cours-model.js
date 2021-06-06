8d014885114d322a3911965b8ab8da11e56fb9fe
IshaqAlathamneh <ishaqalathamneh@gmail.com>
2021-06-06 16:37:46 +0300

    doenm

--- src/model/cours-model.js
+++ src/model/cours-model.js
@@ -4,9 +4,9 @@ const mongoose = require('mongoose');
 
 const course = new mongoose.Schema({
   name: { type: String, required: true, unique: true },
-  assignment: { type: Array, required: true },
-  quiz: { type: Array, required: true },
-  member: { type: Array, required: true },
+  assignments: { type: Array, required: true },
+  quizes: { type: Array, required: true },
+  members: { type: Array, required: true },
   owner: { type: String, required: true },
   description: { type: String, required: true },
   grades: { type: Array, required: true }
