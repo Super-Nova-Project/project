3950d6eded1ee39fbda3a809df90ed6e8f969918
IshaqAlathamneh <ishaqalathamneh@gmail.com>
2021-06-07 15:03:28 +0300

    all

--- src/middleware/getCourseData.js
+++ src/middleware/getCourseData.js
@@ -4,8 +4,13 @@ module.exports = async (req, res, next)=>{
     let courseID = req.params.courseID;
     const course = await courseData.findById(courseID);
     if(course){
-        req.course = course;
-        next();
+        if (course.members.includes(req.user.email)) {
+            
+            req.course = course;
+            next();
+        }else{
+            next('you are not joined to this course')
+        }
     }else{
         next('course ID is not correct');
     }
