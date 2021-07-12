4a8f20152601ce4cae017ad380accaea894b6e93
Musab-Al-omari <musabsomari@gmail.com>
2021-07-12 19:15:30 +0300

    sockit things

--- src/server.js
+++ src/server.js
@@ -9,29 +9,43 @@ const cors = require('cors');
 const morgan = require('morgan');
 const uuid = require('uuid').v4;
 const http = require('http').createServer(app);
-
+const socket = require("socket.io");
 const io = socket(http, {
   cors: {
-      origin: "*",
-      methods: ['GET', 'POST']
+    origin: "*",
+    methods: ['GET', 'POST']
   }
 });
+let rooms = []
+io.on('connection', (socket) => {
+  console.log('connect')
+  socket.on('createCourse', (room) => {
 
+    rooms.push(room)
+  })
+
+  socket.on('give me the rooms',(dd)=>{
+    
+    io.emit('rooms', rooms)
+  })
+
+
+});
 
 
 const User = require('./model/users-model');
 const notFoundHandler = require('./error/not-found');
 const errorHandler = require('./error/error-server');
 const superagent = require('superagent')
-  // const routs = require('./auth/router')
+// const routs = require('./auth/router')
 const basicAuth = require('./middleware/basic')
 const bearerAuth = require('./middleware/bearer')
 const cookieSession = require('cookie-session')
 const courseRouter = require('./routes/course_routes.js');
 const taskRoute = require('./routes/to-do-list-rout')
 require('./middleware/passport')
-  // const courseData = require('./middleware/getCourseData');
-  // const isApproved = require('./middleware/permission');
+// const courseData = require('./middleware/getCourseData');
+// const isApproved = require('./middleware/permission');
 const mongooseCourse = require('./model/cours-model');
 
 app.set('view engine', 'ejs');
@@ -44,74 +58,17 @@ app.use(express.json());
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(cookieSession({
-    name: 'ishaqSession',
-    keys: ['btata1', 'btata2']
-  }))
-  // Process FORM intput and put the data on req.body
+  name: 'ishaqSession',
+  keys: ['btata1', 'btata2']
+}))
+// Process FORM intput and put the data on req.body
 app.use(express.urlencoded({ extended: true }));
 app.use(courseRouter);
 app.use(taskRoute)
 
 
 
-// create sio connection 
 
-const users = {};
-const socketToRoom = {};
-io.on('connection', socket => {
-    socket.on("join room", roomID => {
-        socket.join(roomID)
-        if (users[roomID]) {
-            const length = users[roomID].length;
-            if (length === 4) {
-                socket.emit("room full");
-                return;
-            }
-            users[roomID].push(socket.id);
-        } else {
-            users[roomID] = [socket.id];
-        }
-        socketToRoom[socket.id] = roomID;
-        const usersInThisRoom = users[roomID].filter(id => id !== socket.id);
-
-
-        socket.emit("all users", usersInThisRoom);
-
-        socket.on('startShare', () => {
-            socket.emit("all myUsers", users);
-
-        })
-
-        socket.on("sending signal", payload => {
-            io.to(payload.userToSignal).emit('user joined', { signal: payload.signal, callerID: payload.callerID });
-        });
-
-        socket.on("returning signal", payload => {
-            io.to(payload.callerID).emit('receiving returned signal', { signal: payload.signal, id: socket.id });
-        });
-
-        socket.on('disconnect', () => {
-            console.log('socket.id', socket.id);
-            const roomID = socketToRoom[socket.id];
-            let room = users[roomID];
-            if (room) {
-                room = room.filter(id => id !== socket.id);
-                users[roomID] = room;
-            }
-            socket.broadcast.emit('userLeft', socket.id)
-
-        });
-    })
-    socket.on('chatRoom', roomID => {
-        socket.join(roomID);
-        socket.emit('userId-Joined');
-        socket.on('message', ({ name, message }) => {
-            console.log(message);
-            io.to(roomID).emit('message', { name, message })
-        })
-    })
-
-});
 
 
 
@@ -120,7 +77,7 @@ io.on('connection', socket => {
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html')
 })
-app.post('/signup', async(req, res) => {
+app.post('/signup', async (req, res) => {
   console.log('Body--------->', req.body);
   let user = new User(req.body);
   const userRecord = await user.save();
@@ -170,7 +127,7 @@ app.get('/auth/google/callback', passport.authenticate('google', {
 app.get('/auth/google/failure', (req, res) => {
   res.send('failed login')
 })
-app.get('/auth/google/success', async(req, res) => {
+app.get('/auth/google/success', async (req, res) => {
   try {
     console.log(req.session.passport);
 
