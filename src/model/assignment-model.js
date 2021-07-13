431c4b1c509ad0f296fc0f6308b8cc322e2b2a08
IshaqAlathamneh <ishaqalathamneh@gmail.com>
2021-07-13 07:27:13 +0300

    fix cors

--- src/model/assignment-model.js
+++ src/model/assignment-model.js
@@ -6,7 +6,7 @@ const assignment = new mongoose.Schema({
   assignmentTitle:{type: String, required: true, unique: true},
   assignmentText:{type: String, required: true},
   due_date:{type: Date ,required: true},
-  assignmentFile:{type: File},
+  assignmentFile:{type: Buffer},
   solutionInfo:{type: Array},
   students: { type: Array }
 });
